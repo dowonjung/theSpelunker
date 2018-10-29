50da705062c76c95d3d94821a8ebb50b89afe87e
dowonjung <dowonjung@utexas.edu>
2018-10-29 14:12:13 -0500

    tilemap change

--- states/State0.js
+++ states/State0.js
@@ -24,10 +24,12 @@ var state0 = {
         player.body.collideWorldBounds = true;
         game.camera.follow(player);
         
-        isDamaged = -1;
         alive = true;
+        HP = 5;
         type = 'player';
         fireCoolDown = 0;
+        timer = 0;
+        this.checkTimer();
         
         // Keyboard
         cursors = game.input.keyboard.createCursorKeys();
@@ -36,15 +38,23 @@ var state0 = {
         fireButton = game.input.keyboard.addKey(Phaser.Keyboard.S);
         accessMenuButton = game.input.keyboard.addKey(Phaser.Keyboard.A);
         
+        // Boss Slime
+        bossSlime = game.add.sprite(600, 600, 'slimeMove');
+        bossSlime.animations.add('slimeWalk', [0, 1, 2, 3], 1, true);
+        bossSlime.anchor.setTo(0.5, 0.5);
+        bossSlime.scale.setTo(0.2, 0.2);
+        game.physics.enable(bossSlime);
+        bossSlime.body.collideWorldBounds = true;
+        bossSlime.health = 50;
     },
 
     update: function() {
-        //Player Movement
+        // Player Movement
         game.physics.arcade.collide(player, ground);
         
         if (cursors.left.isDown) {
             player.body.velocity.x = -100;
-            player.scale.setTo(0.1, 0.1);
+            player.scale.setTo(-0.1, 0.1);
             player.play('run');
         
         } else if (cursors.right.isDown) {
@@ -59,12 +69,52 @@ var state0 = {
         }
         
         if (jumpButton.isDown && player.body.onFloor()) {
-            player.body.velocity.y = -150;
+            player.body.velocity.y = -175;
             player.scale.setTo(0.1, 0.1);
         }
+        
+        // Boss Slime Movement
+        game.physics.arcade.collide(bossSlime, ground);
+        if (player.x <= bossSlime.x){
+            if (game.time.now - timer > 5000){
+                bossSlime.body.velocity.x = -50;
+                bossSlime.scale.setTo(-0.2, 0.2);
+                bossSlime.play('slimeWalk');
+            }
+        } else {
+            if (game.time.now - timer > 5000){
+                bossSlime.body.velocity.x = 50;
+                bossSlime.scale.setTo(0.2, 0.2);
+                bossSlime.play('slimeWalk');
+            }
+        }
+        
+        // Boss Slime Attack
+        game.time.events.loop(Phaser.Time.SECOND*8, bossSlime.bossSlimeAttack, this);
         
         /*if (GAME_OVER) {
             game.state.start('credits');
         }*/
+    },
+    
+    bossSlimeAttack: function(){
+        var bossSlimeAttack = game.add.sprite('slimeAttack');
+        bossSlimeAttack.animations.add('slimeAttack1', [0, 1, 2], 20, true);
+        if (player.x <= bossSlime.x){
+            bossSlimeAttack.scale.setTo(-0.2, 0.2);
+            bossSlimeAttack.play('slimeAttack');
+        } else {
+            bossSlimeAttack.scale.setTo(0.2, 0.2);
+            bossSlimeAttack.play('slimeAttack');
+        }
+        game.physics.arcade.overlap(player, bossSlimeAttack, isDamaged, null, this);
+    },
+    
+    isDamaged: function(){
+        HP -= 1;
+    },
+    
+    checkTimer: function(){
+        timer = game.time.now;
     }
 };
